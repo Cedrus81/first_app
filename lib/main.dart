5618e5f26f2baa84f4b617028aedd167b4c64404
Cedrus81 <erez.eitan.97@gmail.com>
2023-06-19 17:49:57 +0300

    divided widgets to diferent files

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
       ),
     ),
   );
-}
-
-class GradientContainer extends StatelessWidget {
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 197, 172, 241),
-            Colors.blueAccent,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            fontSize: 40,
-            color: Color.fromARGB(255, 202, 29, 43),
-          ),
-        ),
-      ),
-    );
-  }
 }
