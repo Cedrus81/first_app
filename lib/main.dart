20bb3cee689ef01f165af066429e1be753330887
Cedrus81 <erez.eitan.97@gmail.com>
2023-06-19 17:19:08 +0300

    made a custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 197, 172, 241),
+            Colors.blueAccent,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontSize: 40,
+            color: Color.fromARGB(255, 202, 29, 43),
+          ),
+        ),
+      ),
+    );
+  }
 }
