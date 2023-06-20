30a7432fc29bbd1e74ce57a04b3cffa3566bc7d3
Cedrus81 <erez.eitan.97@gmail.com>
2023-06-20 09:31:51 +0300

    added images, made gradient container more dynamic

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
+  final List<Color> colors;
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 197, 172, 241),
-            Colors.blueAccent,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyledText(),
+        child: StyledText('hola world'),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:first_app/gradient_container.dart';
 
 void main() {
+  const colors = [
+    Color.fromARGB(255, 197, 172, 241),
+    Colors.blueAccent,
+  ];
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(colors),
       ),
     ),
   );
